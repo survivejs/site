56aabb490d2541022557839ee50095c17e0579e8
Juho Vepsalainen <bebraw@gmail.com>
2015-09-04 10:39:43 +0300

    Try to load cf config from another js module
    Easier to maintain this way.

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,9 @@
-module.exports = {
-  email: process.env.CF_EMAIL,
-  key: process.env.CF_KEY
-};
+try {
+  module.exports = require('./site-config');
+}
+catch(e) {
+  module.exports = {
+    email: process.env.CF_EMAIL,
+    key: process.env.CF_KEY
+  };
+}
