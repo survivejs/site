5c6b22c46444a2ca9e6074428335f93e59f3f01a
Juho Vepsalainen <bebraw@gmail.com>
2015-09-17 09:55:57 +0300

    Push Markdown bits to a utility module
    This way it's easier to work with the configuration and share code
    between projects.

--- antwar.config.js
+++ antwar.config.js
@@ -4,50 +4,11 @@ var path = require('path');
 var React = require('react');
 var _ = require('lodash');
 var removeMd = require('remove-markdown');
-var marked = require('marked');
-var renderer = new marked.Renderer();
 var rssPlugin = require('antwar-rss-plugin');
 var highlightPlugin = require('antwar-highlight-plugin');
 var prevnextPlugin = require('antwar-prevnext-plugin');
 
-// alter marked renderer to add slashes to beginning so images point at root
-// leanpub expects images without slash...
-renderer.image = function(href, title, text) {
-  return '<img src="/' + href + '" alt="' + text + '">';
-};
-
-// patch ids (this.options.headerPrefix can be undefined!)
-renderer.heading = function(text, level, raw) {
-  var id = raw.toLowerCase().replace(/[^\w]+/g, '-');
-
-  return '<a class="header-anchor" href="#' + id + '" id="' + id + '"></a>'
-    + '<h'
-    + level
-    + '>'
-    + text
-    + '<a class="header-anchor-select" href="#' + id + '">#</a>'
-    + '</h'
-    + level
-    + '>\n';
-};
-
-var markedDefaults = {
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: false,
-  sanitizer: null,
-  mangle: true,
-  smartLists: false,
-  silent: false,
-  highlight: null,
-  langPrefix: 'lang-',
-  smartypants: false,
-  headerPrefix: '',
-  renderer: renderer,
-  xhtml: false
-};
+var markdown = require('./utils/markdown');
 
 module.exports = {
   assets: [
@@ -180,9 +141,8 @@ function blog() {
       },
       content: function(o) {
         var content = o.file.__content.split('\n').slice(1).join('\n');
-        var tokens = parseQuotes(content);
 
-        return marked.parser(tokens, markedDefaults);
+        return markdown.process(content);
       }
     },
     layout: 'blog',
@@ -229,9 +189,8 @@ function webpackReact() {
       },
       content: function(o) {
         var content = o.file.__content.split('\n').slice(1).join('\n');
-        var tokens = parseQuotes(content);
 
-        return marked.parser(tokens, markedDefaults);
+        return markdown.process(content);
       },
       preview: function(o) {
         var previewLimit = 300;
@@ -307,34 +266,6 @@ function webpackReact() {
       return ret;
     },
   };
-}
-
-function parseQuotes(data) {
-  var tokens = marked.lexer(data).map(function(t) {
-    if(t.type === 'paragraph') {
-      return parseCustomQuote(t, 'T>', 'tip') ||
-        parseCustomQuote(t, 'W>', 'warning') ||
-        t;
-    }
-
-    return t;
-  });
-  tokens.links = [];
-
-  return tokens;
-}
-
-function parseCustomQuote(token, match, className) {
-  if(token.type === 'paragraph') {
-    var text = token.text;
-
-    if(text.indexOf(match) === 0) {
-      return {
-        type: 'html',
-        text: '<blockquote class="' + className + '">' + text.slice(2).trim() + '</blockquote>',
-      };
-    }
-  }
 }
 
 function id(a) {return a;}
---
+++ utils/markdown.js
@@ -0,0 +1,76 @@
+'use strict';
+var marked = require('marked');
+var renderer = new marked.Renderer();
+
+// alter marked renderer to add slashes to beginning so images point at root
+// leanpub expects images without slash...
+renderer.image = function(href, title, text) {
+  return '<img src="/' + href + '" alt="' + text + '">';
+};
+
+// patch ids (this.options.headerPrefix can be undefined!)
+renderer.heading = function(text, level, raw) {
+  var id = raw.toLowerCase().replace(/[^\w]+/g, '-');
+
+  return '<a class="header-anchor" href="#' + id + '" id="' + id + '"></a>'
+    + '<h'
+    + level
+    + '>'
+    + text
+    + '<a class="header-anchor-select" href="#' + id + '">#</a>'
+    + '</h'
+    + level
+    + '>\n';
+};
+
+var markedDefaults = {
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: false,
+  sanitizer: null,
+  mangle: true,
+  smartLists: false,
+  silent: false,
+  highlight: null,
+  langPrefix: 'lang-',
+  smartypants: false,
+  headerPrefix: '',
+  renderer: renderer,
+  xhtml: false
+};
+
+exports.process = function(content) {
+  var tokens = parseQuotes(content);
+
+  return marked.parser(tokens, markedDefaults);
+};
+
+function parseQuotes(data) {
+  var tokens = marked.lexer(data).map(function(t) {
+    if(t.type === 'paragraph') {
+      return parseCustomQuote(t, 'T>', 'tip') ||
+        parseCustomQuote(t, 'W>', 'warning') ||
+        t;
+    }
+
+    return t;
+  });
+  tokens.links = [];
+
+  return tokens;
+}
+
+function parseCustomQuote(token, match, className) {
+  if(token.type === 'paragraph') {
+    var text = token.text;
+
+    if(text.indexOf(match) === 0) {
+      return {
+        type: 'html',
+        text: '<blockquote class="' + className + '">' + text.slice(2).trim() + '</blockquote>',
+      };
+    }
+  }
+}
