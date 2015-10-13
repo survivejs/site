5ed8e8e6f4db1e70bc92c2ba481c5a2b7493d2a5
Juho Vepsalainen <bebraw@gmail.com>
2015-10-13 10:58:12 +0300

    teaser - Tidy up

--- layouts/Teaser.jsx
+++ layouts/Teaser.jsx
@@ -11,16 +11,22 @@ module.exports = React.createClass({
         var amount = this.props.amount;
         var posts = this.getSectionItems(section).slice(0, amount);
 
+        return posts.length > 1 ?
+            this.renderMultiple(posts) :
+            this.renderSingle(posts[0]);
+    },
+    renderMultiple(posts) {
         return (
             <ul className='blog-teasers'>
                 {posts.map((post, i) => {
-                    return (<li key={'post' + i}>
-                        <a className='blog-teaser' href={post.url}>
-                            {post.title}
-                        </a>
-                    </li>);
+                    return <li key={'post' + i}>{this.renderSingle(post)}</li>;
                 })}
             </ul>
         );
+    },
+    renderSingle(post) {
+        return <a className='blog-teaser' href={post.url}>
+            {post.title}
+        </a>;
     }
 });
