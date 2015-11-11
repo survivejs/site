cb98bc1481ae0d0a2a7ebd59726866d340aec762
Juho Vepsalainen <bebraw@gmail.com>
2015-11-11 15:47:30 +0200

    Simplify cloudflare purge script
    This might work a little better.

--- utils/purge_cf.js
+++ utils/purge_cf.js
@@ -1,78 +1,28 @@
 'use strict';
-var url = require('url');
+var CloudFlareAPI = require('cloudflare4');
 
-var _ = require('lodash');
-var request = require('request');
 var config = require('./config');
-
-var API_ROOT = 'https://api.cloudflare.com/client/v4/';
 
 main();
 
 function main() {
-  getZones(config, function(err, res) {
-    if(err) {
-      return console.error(err);
-    }
+  var api = new CloudFlareAPI(config);
 
-    var zone = _.findWhere(res, {
-      name: 'survivejs.com'
-    })
+  api.zoneGetAll({
+    name: 'survivejs.com'
+  }).then(function(zones) {
+    var zone = zones[0];
 
-    if(zone && zone.id) {
-      purgeCache(_.assign(config, {
-        zoneId: zone.id,
-        files: [
-          'http://survivejs.com/assets/main.css'
-        ]
-      }), function(err, res) {
-        if(err) {
-          return console.error(err);
-        }
-
-        console.log('purged cache', res);
-      })
-    }
-    else {
-      console.error('no zone found!');
-    }
-  });
-}
-
-function getZones(config, cb) {
-  request({
-    url: url.resolve(API_ROOT, 'zones'),
-    headers: {
-      'X-Auth-Email': config.email,
-      'X-Auth-Key': config.key
-    },
-    json: true
-  }, function(err, res) {
-    if(err) {
-      return cb(err);
-    }
-
-    cb(null, res.body.result);
-  });
-}
-
-function purgeCache(config, cb) {
-  request({
-    url: url.resolve(API_ROOT, 'zones/' + config.zoneId + '/purge_cache'),
-    method: 'delete',
-    headers: {
-      'X-Auth-Email': config.email,
-      'X-Auth-Key': config.key
-    },
-    body: {
-      files: config.files
-    },
-    json: true
-  }, function(err, res) {
-    if(err) {
-      return cb(err);
-    }
-
-    cb(null, res.body);
+    api.zonePurgeCacheBy(zone.id, {
+      files: [
+        'http://survivejs.com/assets/main.css'
+      ]
+    }).then(function(ok) {
+      console.log('purged main.css');
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }).catch(function(err) {
+    console.error(err);
   });
 }
