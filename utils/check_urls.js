5648523283f4f03788caf45af5b9604ec8dc6bbf
Juho Vepsalainen <bebraw@gmail.com>
2015-11-30 15:21:54 +0200

    Set up initial 404 checker script

---
+++ utils/check_urls.js
@@ -0,0 +1,35 @@
+'use strict';
+var express = require('express')
+var serveStatic = require('serve-static')
+var BrokenLinksChecker = require('bs-broken-links-checker').BrokenLinksChecker;
+var portfinder = require('portfinder');
+
+main();
+
+function main() {
+  portfinder.getPort(function (err, port) {
+    if(err) {
+      return console.error(err);
+    }
+
+    var app = express();
+    app.use(serveStatic('build/'));
+
+    var server = app.listen(port, function(err) {
+      if(err) {
+        return console.error(err);
+      }
+
+      var brokenLinksChecker = new BrokenLinksChecker({
+        logger: console.error.bind(console),
+        onDone: function() {
+          console.log('done');
+
+          //server.close();
+        }
+      });
+
+      brokenLinksChecker.start('http://localhost:' + port);
+    });
+  });
+}
