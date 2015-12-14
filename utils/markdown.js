d573164675cd78afa0d398caf0eb24bc1e94a6c8
Juho Vepsalainen <bebraw@gmail.com>
2015-12-14 17:56:29 +0200

    Tidy up Prism bits
    There's still something weird going on if you navigate to a page with
    custom highlighting, to somewhere else, and back. It loses the custom
    formatting in this case.
    
    This fails with build too. Maybe fixing that will help with the dev
    glitch.

--- utils/markdown.js
+++ utils/markdown.js
@@ -3,73 +3,10 @@ var marked = require('marked');
 var Prism = require('prismjs');
 var languages = require('prism-languages');
 
+attachPrismHooks(Prism);
+
 var highlight = Prism.highlight;
 var renderer = new marked.Renderer();
-
-// detect leanpub specific syntax
-Prism.hooks.add('before-highlight', function(env) {
-  env.tokenStack = {
-    startInsert: 0,
-    endInsert: 0,
-    startDelete: 0,
-    endDelete: 0
-  };
-
-  env.code = env.code.replace(/leanpub-start-insert/ig, function(match) {
-    env.tokenStack.startInsert++;
-
-    return '_LEANPUB_START_INSERT' + env.tokenStack.startInsert;
-  });
-
-  env.code = env.code.replace(/leanpub-end-insert/ig, function(match) {
-    env.tokenStack.endInsert++;
-
-    return '_LEANPUB_END_INSERT' + env.tokenStack.endInsert;
-  });
-
-  env.code = env.code.replace(/leanpub-start-delete/ig, function(match) {
-    env.tokenStack.startDelete++;
-
-    return '_LEANPUB_START_DELETE' + env.tokenStack.startDelete;
-  });
-
-  env.code = env.code.replace(/leanpub-end-delete/ig, function(match) {
-    env.tokenStack.endDelete++;
-
-    return '_LEANPUB_END_DELETE' + env.tokenStack.endDelete;
-  });
-});
-Prism.hooks.add('after-highlight', function(env) {
-  for(var i = 0, t = env.tokenStack.startInsert; i < t; i++) {
-    env.highlightedCode = env.highlightedCode.replace(
-      '_LEANPUB_START_INSERT' + (i + 1),
-      '<div class="leanpub-insert">'
-    );
-  }
-
-  for(var i = 0, t = env.tokenStack.endInsert; i < t; i++) {
-    env.highlightedCode = env.highlightedCode.replace(
-      '_LEANPUB_END_INSERT' + (i + 1),
-      '</div>'
-    );
-  }
-
-  for(var i = 0, t = env.tokenStack.startDelete; i < t; i++) {
-    env.highlightedCode = env.highlightedCode.replace(
-      '_LEANPUB_START_DELETE' + (i + 1),
-      '<div class="leanpub-delete">'
-    );
-  }
-
-  for(var i = 0, t = env.tokenStack.endDelete; i < t; i++) {
-    env.highlightedCode = env.highlightedCode.replace(
-      '_LEANPUB_END_DELETE' + (i + 1),
-      '</div>'
-    );
-  }
-
-  env.element.innerHTML = env.highlightedCode;
-});
 
 // alter marked renderer to add slashes to beginning so images point at root
 // leanpub expects images without slash...
@@ -141,6 +78,73 @@ function parseQuotes(data) {
   tokens.links = [];
 
   return tokens;
+}
+
+function attachPrismHooks(Prism) {
+  // detect leanpub specific syntax
+  Prism.hooks.add('before-highlight', function(env) {
+    env.tokenStack = {
+      startInsert: 0,
+      endInsert: 0,
+      startDelete: 0,
+      endDelete: 0
+    };
+
+    env.code = env.code.replace(/leanpub-start-insert/ig, function(match) {
+      env.tokenStack.startInsert++;
+
+      return '_LEANPUB_START_INSERT' + env.tokenStack.startInsert;
+    });
+
+    env.code = env.code.replace(/leanpub-end-insert/ig, function(match) {
+      env.tokenStack.endInsert++;
+
+      return '_LEANPUB_END_INSERT' + env.tokenStack.endInsert;
+    });
+
+    env.code = env.code.replace(/leanpub-start-delete/ig, function(match) {
+      env.tokenStack.startDelete++;
+
+      return '_LEANPUB_START_DELETE' + env.tokenStack.startDelete;
+    });
+
+    env.code = env.code.replace(/leanpub-end-delete/ig, function(match) {
+      env.tokenStack.endDelete++;
+
+      return '_LEANPUB_END_DELETE' + env.tokenStack.endDelete;
+    });
+  });
+  Prism.hooks.add('after-highlight', function(env) {
+    for(var i = 0, t = env.tokenStack.startInsert; i < t; i++) {
+      env.highlightedCode = env.highlightedCode.replace(
+        '_LEANPUB_START_INSERT' + (i + 1),
+        '<div class="leanpub-insert">'
+      );
+    }
+
+    for(var i = 0, t = env.tokenStack.endInsert; i < t; i++) {
+      env.highlightedCode = env.highlightedCode.replace(
+        '_LEANPUB_END_INSERT' + (i + 1),
+        '</div>'
+      );
+    }
+
+    for(var i = 0, t = env.tokenStack.startDelete; i < t; i++) {
+      env.highlightedCode = env.highlightedCode.replace(
+        '_LEANPUB_START_DELETE' + (i + 1),
+        '<div class="leanpub-delete">'
+      );
+    }
+
+    for(var i = 0, t = env.tokenStack.endDelete; i < t; i++) {
+      env.highlightedCode = env.highlightedCode.replace(
+        '_LEANPUB_END_DELETE' + (i + 1),
+        '</div>'
+      );
+    }
+
+    env.element.innerHTML = env.highlightedCode;
+  });
 }
 
 function parseCustomQuote(token, match, className) {
