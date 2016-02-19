1fb5f267c10c7281287e7a5b87a745d2d75b5f51
Juho Vepsalainen <bebraw@gmail.com>
2016-02-19 17:34:01 +0200

    Drop flexbox
    Now the frontpage looks mostly ok in Chrome and Firefox. Still some work
    left to do, though.

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,20 +9,20 @@ var Index = React.createClass({
     return (
       <div className='frontpage'>
         <div className='front__heading'>
-          <div className='front-header-wrapper'>
-            <h1 className='front-header'>Learn Webpack and React</h1>
-            <h3 className='front-motto'>SurviveJS will take you from</h3>
-            <h3 className='front-motto'>apprentice to master</h3>
+          <div className='front-heading-content-wrapper'>
+            <div className='front-header-wrapper'>
+              <h1 className='front-header'>Learn Webpack and React</h1>
+              <h3 className='front-motto'>SurviveJS will take you from</h3>
+              <h3 className='front-motto'>apprentice to master</h3>
 
-            <div className='front-button-wrapper'>
-              <a className='btn btn--buy' href='https://leanpub.com/survivejs_webpack_react'>Become a master</a>
-              <div className='read-free'>
-                <span>or <a href='/webpack_react/introduction'>read the free version</a></span>
+              <div className='front-button-wrapper'>
+                <a className='btn btn--buy' href='https://leanpub.com/survivejs_webpack_react'>Become a master</a>
+                <div className='read-free'>
+                  <span>or <a href='/webpack_react/introduction'>read the free version</a></span>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className='front-cover-wrapper'>
             <img className='front-cover' src='images/title_page_small.png' width='255' height='329' />
           </div>
         </div>
