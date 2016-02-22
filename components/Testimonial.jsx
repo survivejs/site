de03b6e7a5b211daeae2a44ea062c3eb91d4d0f5
Juho Vepsalainen <bebraw@gmail.com>
2016-02-22 19:04:30 +0200

    Push testimonial name/title below bubbles
    A little neater this way.

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -5,12 +5,6 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className='testimonial'>
-        <div className='name-wrapper'>
-          <span className='name'>{this.props.name}</span>
-        </div>
-        <div className='title-wrapper'>
-          <span className='title'>{this.props.title || '\u00a0'}</span>
-        </div>
         <div className='text-wrapper'>
           <span className='text'>{this.props.text}</span>
         </div>
@@ -18,6 +12,12 @@ module.exports = React.createClass({
           <a href={this.props.url} target='_blank'>
             <img className='photo' src={this.props.image} alt={this.props.name} />
           </a>
+        </div>
+        <div className='name-wrapper'>
+          <span className='name'>{this.props.name}</span>
+        </div>
+        <div className='title-wrapper'>
+          <span className='title'>{this.props.title || '\u00a0'}</span>
         </div>
       </div>
     );
