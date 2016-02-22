0235c74a05bdd1bfd8d2a687e620c54ff0a1794e
Juho Vepsalainen <bebraw@gmail.com>
2016-02-22 16:53:48 +0200

    Improve subscription form

--- components/SocialLinks.jsx
+++ components/SocialLinks.jsx
@@ -11,6 +11,9 @@ module.exports = React.createClass({
         <blockquote className="tip">
           {`${start} subscribing to the mailing list below or following `}
           <a href="https://twitter.com/survivejs">@survivejs</a> for occasional updates. There is also <a href="/atom.xml">RSS</a> available for old beards (no pun intended).</blockquote>
+
+        <h2 className="subscribe-header">Subscribe to the mailing list</h2>
+
         <form action="//jster.us7.list-manage.com/subscribe/post?u=ed40c0084a0c5ba31b3365d65&amp;id=b853b8e786"
           method="post" id="mc-embedded-subscribe-form"
           name="mc-embedded-subscribe-form"
