58b1d8a21dea7a709e07cb173f14b632f9081f27
Juho Vepsalainen <bebraw@gmail.com>
2016-02-22 17:19:21 +0200

    Fix asset paths
    The problem here is that Webpack `require` doesn't work with Antwar as
    expected. It would be better to load all assets through Webpack instead
    of copying. Now there's a conflict due to that. Alternatively it could
    be set up so that both are allowed to write to the same directories.

--- pages/index.jsx
+++ pages/index.jsx
@@ -34,7 +34,7 @@ var Index = React.createClass({
         <div className='front__testimonials'>
           <Testimonial
             text='SurviveJS connects the dots and fills the gaps left by the latest JavaScript tools and their documentation. With its detailed step-by-step tutorial approach, SurviveJS gives practical examples of code and configuration while providing context for choosing one method over another.'
-            image={require('../assets/img/testimonials/steve.jpg')}
+            image={'/assets/img/testimonials/steve.jpg'}
             name='Steve Piercy'
             title=''
             url='http://www.StevePiercy.com'
@@ -42,7 +42,7 @@ var Index = React.createClass({
 
           <Testimonial
             text='Webpack is powerful but configuring it can be painful. Same goes with React. There are so many ways of configuring React with asset compilation, minification etc that it is easy to get lost. This book provides practical tips on how to get configure Webpack to make it work with React and other modern JavaScript tools like Babel.'
-            image={require('../assets/img/testimonials/raj.jpg')}
+            image={'/assets/img/testimonials/raj.jpg'}
             name='Neeraj Singh'
             title='Founder of BigBinary'
             url='http://bigbinary.com/'
@@ -50,7 +50,7 @@ var Index = React.createClass({
 
           <Testimonial
             text='Setting up a cutting-edge frontend development environment with React takes time. What sets SurviveJS apart is that it does not only show you exactly how to do it, it evolves, preventing JS fatigue, hence the name.'
-            image={require('../assets/img/testimonials/omar.jpg')}
+            image={'/assets/img/testimonials/omar.jpg'}
             name='Omar van Galen'
             title='Intrasoft'
             url='http://www.intrasoft.nl/'
@@ -58,7 +58,7 @@ var Index = React.createClass({
 
           <Testimonial
             text='This is the most useful book I’ve read this year. Apart from React itself it gives you a headstart into Webpack which is gaining traction as the weapon of choice for deploying Javascript apps and Flux architecture pattern. Well-written, full of annotated code, what’s not to like? Yes, I would recommend it to a friend.'
-            image={require('../assets/img/testimonials/nick.jpg')}
+            image={'/assets/img/testimonials/nick.jpg'}
             name='Nick Ostrovsky'
             title=''
             url='http://firedev.com/'
@@ -66,7 +66,7 @@ var Index = React.createClass({
 
           <Testimonial
             text="I originally got this book to learn React, but I found quickly that the instruction on Webpack alone was worth the price of admission. I have followed this book's iteration from very early on, and it gets better every time. It is very useful, and to this day I keep going back to it as a good way to get new projects up and running."
-            image={require('../assets/img/testimonials/phil.jpg')}
+            image={'/assets/img/testimonials/phil.jpg'}
             name='Phil Ledgerwood'
             title='Integrity Inspired Solutions'
             url='http://www.integrityinspired.com/'
