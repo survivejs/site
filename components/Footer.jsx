2946c9b1ca7a43dc1aa030e09bdd2d04218978dc
Juho Vepsalainen <bebraw@gmail.com>
2016-02-23 13:51:14 +0200

    Improve Footer code indentation

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,48 +2,47 @@ var React = require('react');
 var Teaser = require('./Teaser');
 
 module.exports = React.createClass({
-    displayName: 'Footer',
+  displayName: 'Footer',
+  render: function() {
+    const props = this.props;
+    const section = props.section;
+    const pages = section.pages ? section.pages('blog').slice(0, 10) : [];
 
-    render: function() {
-        const props = this.props;
-        const section = props.section;
-        const pages = section.pages ? section.pages('blog').slice(0, 10) : [];
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <div className='footer-content-wrapper'>
+            <div className='footer-social'>
+              <h3>Social</h3>
+              <ul>
+                <li>
+                  <a href='https://twitter.com/survivejs' target='_blank'>@survivejs</a>
+                </li>
+                <li>
+                  <a href='http://eepurl.com/bth1v5' target='_blank'>Mailing List</a>
+                </li>
+                <li>
+                  <a href='https://gitter.im/survivejs/webpack_react' target='_blank'>Gitter Chat</a>
+                </li>
+                <li>
+                  <a href='https://github.com/survivejs/webpack_react' target='_blank'>GitHub</a>
+                </li>
+                <li>
+                  <a href='/atom.xml' target='_blank'>Blog RSS</a>
+                </li>
+                <li>
+                  <a href='http://goo.gl/forms/OWdGIOdHm9' target='_blank'>Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className='footer-blog'>
+              <h3>From the Blog</h3>
 
-        return (
-            <footer>
-                <div className='footer-wrapper'>
-                    <div className='footer-content-wrapper'>
-                        <div className='footer-social'>
-                            <h3>Social</h3>
-                            <ul>
-                                <li>
-                                    <a href='https://twitter.com/survivejs' target='_blank'>@survivejs</a>
-                                </li>
-                                <li>
-                                    <a href='http://eepurl.com/bth1v5' target='_blank'>Mailing List</a>
-                                </li>
-                                <li>
-                                    <a href='https://gitter.im/survivejs/webpack_react' target='_blank'>Gitter Chat</a>
-                                </li>
-                                <li>
-                                    <a href='https://github.com/survivejs/webpack_react' target='_blank'>GitHub</a>
-                                </li>
-                                <li>
-                                    <a href='/atom.xml' target='_blank'>Blog RSS</a>
-                                </li>
-                                <li>
-                                    <a href='http://goo.gl/forms/OWdGIOdHm9' target='_blank'>Contact</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className='footer-blog'>
-                            <h3>From the Blog</h3>
-
-                            <Teaser pages={pages} />
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        );
-    }
+              <Teaser pages={pages} />
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 });
