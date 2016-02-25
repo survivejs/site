e06bcf7b53424af8d62405640a1ed28330cea4c1
Juho Vepsalainen <bebraw@gmail.com>
2016-02-25 11:23:41 +0200

    Simplify highlighting logic
    Less dependency on Prism. Not ideal yet, though, but better than before.

--- utils/highlight.js
+++ utils/highlight.js
@@ -10,7 +10,7 @@ module.exports = function(code, language) {
   try {
     language = language || 'bash';
 
-    return highlight(code, languages[language]);
+    return leanpubify(highlight(code, languages[language]));
   }
   catch(err) {
     console.warn('Failed to highlight', language, code, err);
@@ -22,68 +22,28 @@ module.exports = function(code, language) {
 function attachPrismHooks(Prism) {
   // detect leanpub specific syntax
   Prism.hooks.add('before-highlight', function(env) {
-    env.tokenStack = {
-      startInsert: 0,
-      endInsert: 0,
-      startDelete: 0,
-      endDelete: 0
-    };
-
     env.code = env.code.replace(/leanpub-start-insert/ig, function(match) {
-      env.tokenStack.startInsert++;
-
-      return '_LEANPUB_START_INSERT' + env.tokenStack.startInsert;
+      return '_LEANPUB_START_INSERT';
     });
 
     env.code = env.code.replace(/leanpub-end-insert/ig, function(match) {
-      env.tokenStack.endInsert++;
-
-      return '_LEANPUB_END_INSERT' + env.tokenStack.endInsert;
+      return '_LEANPUB_END';
     });
 
     env.code = env.code.replace(/leanpub-start-delete/ig, function(match) {
-      env.tokenStack.startDelete++;
-
-      return '_LEANPUB_START_DELETE' + env.tokenStack.startDelete;
+      return '_LEANPUB_START_DELETE';
     });
 
     env.code = env.code.replace(/leanpub-end-delete/ig, function(match) {
-      env.tokenStack.endDelete++;
-
-      return '_LEANPUB_END_DELETE' + env.tokenStack.endDelete;
+      return '_LEANPUB_END';
     });
   });
-  Prism.hooks.add('after-highlight', function(env) {
-    for(var i = 0, t = env.tokenStack.startInsert; i < t; i++) {
-      env.highlightedCode = env.highlightedCode.replace(
-        '_LEANPUB_START_INSERT' + (i + 1),
-        '<div class="leanpub-insert">'
-      );
-    }
-
-    for(var i = 0, t = env.tokenStack.endInsert; i < t; i++) {
-      env.highlightedCode = env.highlightedCode.replace(
-        '_LEANPUB_END_INSERT' + (i + 1),
-        '</div>'
-      );
-    }
-
-    for(var i = 0, t = env.tokenStack.startDelete; i < t; i++) {
-      env.highlightedCode = env.highlightedCode.replace(
-        '_LEANPUB_START_DELETE' + (i + 1),
-        '<div class="leanpub-delete">'
-      );
-    }
-
-    for(var i = 0, t = env.tokenStack.endDelete; i < t; i++) {
-      env.highlightedCode = env.highlightedCode.replace(
-        '_LEANPUB_END_DELETE' + (i + 1),
-        '</div>'
-      );
-    }
-
-    if(env.element) {
-      env.element.innerHTML = env.highlightedCode;
-    }
-  });
 }
+
+function leanpubify(code) {
+  return code.replace(/_LEANPUB_START_INSERT/g, '<div class="leanpub-insert">')
+    .replace(/_LEANPUB_START_DELETE/g, '<div class="leanpub-delete">')
+    .replace(/_LEANPUB_END/g, '</div>');
+}
+
+module.exports.leanpubify = leanpubify;
