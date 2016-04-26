fecad03a25487e5ec00fd5114663149585dbe70b
Juho Vepsalainen <bebraw@gmail.com>
2016-04-26 19:47:11 +0300

    Set up redirects from /webpack_react to /react

--- antwar.config.js
+++ antwar.config.js
@@ -148,6 +148,10 @@ function blog() {
   };
 }
 
+function react(headers) {
+  // TODO: set this up
+}
+
 function webpackReact(headers) {
   return {
     title: 'Table of Contents',
@@ -269,6 +273,30 @@ function webpackReact(headers) {
       page: function() {
         return require('./layouts/ChapterPage.jsx').default;
       }
+    },
+    redirects: {
+      'introduction': '/react/introduction',
+      'webpack': '/webpack/introduction',
+      'webpack_compared': '/webpack/webpack-compared',
+      'developing_with_webpack': '/webpack/developing-with-webpack',
+      'webpack_and_react': '/webpack/advanced-techniques/configuring-react',
+      'react': '/react/implementing-kanban',
+      'implementing_notes': '/react/implementing-kanban/implementing-notes',
+      'react_and_flux': '/react/implementing-kanban/react-and-flux',
+      'from_notes_to_kanban': '/react/implementing-kanban/from-notes-to-kanban',
+      'implementing_dnd': '/react/implementing-kanban/drag-and-drop',
+      'building_kanban': '/webpack/building-with-kanban',
+      'advanced': '/react/advanced-techniques',
+      'testing_react': '/react/advanced-techniques/testing-react',
+      'typing_with_react': '/react/advanced-techniques/typing-with-react',
+      'linting_in_webpack': '/webpack/advanced-techniques/linting',
+      'authoring_libraries': '/webpack/advanced-techniques/authoring-packages',
+      'styling_react': '/react/advanced-techniques/styling-react',
+      'appendices': '/react/appendices',
+      'structuring_react_projects': '/react/advanced-techniques/structuring-react-projects',
+      'language_features': '/react/appendices/language-features',
+      'understanding_decorators': '/react/appendices/understanding-decorators',
+      'troubleshooting': '/react/appendices/troubleshooting'
     }
   };
 }
