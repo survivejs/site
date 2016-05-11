80280204725127affb9ffe4cfc8ee36cc156f048
Juho Vepsalainen <bebraw@gmail.com>
2016-05-11 09:44:43 +0300

    Fix 1.2.0 path

--- antwar.config.js
+++ antwar.config.js
@@ -141,6 +141,7 @@ function blog() {
       }
     },
     redirects: {
+      'survivejs-webpack120': 'survivejs-webpack-120',
       'mobservable-interview': 'mobx-interview',
       'react-router5': 'react-router5-interview'
     }
