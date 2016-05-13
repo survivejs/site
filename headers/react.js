4a4a341f3c44a0befc27b9d91a7dde9c4ca6330b
Juho Vepsalainen <bebraw@gmail.com>
2016-05-13 10:13:17 +0300

    Update react book header images

--- headers/react.js
+++ headers/react.js
@@ -1,4 +1,5 @@
 module.exports = [
+  /* introduction */
   {
     "author": "James Jordan",
     "source": "https://www.flickr.com/photos/69826987@N00/1501451576",
@@ -6,13 +7,13 @@ module.exports = [
     "image": "journey.jpg",
     "type": "intro"
   },
+  /* Part: getting started */
   {
-    "author": "Radek Orszewski",
-    "source": "https://www.flickr.com/photos/61745819@N00/10588140955",
-    "license": "CC BY-NC-ND",
-    "image": "kanban_photo.jpg",
+    "license": "Public Domain",
+    "image": "shape_sorter.jpg",
     "type": "part"
   },
+  /* introduction to react */
   {
     "author": "Efraimstochter",
     "source": "https://pixabay.com/en/wheels-machine-installation-art-784865/",
@@ -20,6 +21,7 @@ module.exports = [
     "image": "wheels.jpg",
     "type": "chapter"
   },
+  /* implementing notes */
   {
     "author": "Angela",
     "source": "https://www.flickr.com/photos/73635318@N00/2663422833",
@@ -28,6 +30,7 @@ module.exports = [
     "demo": "04_implementing_notes",
     "type": "chapter"
   },
+  /* styling notes */
   {
     "author": "Philip Brown",
     "source": "https://www.flickr.com/photos/67478016@N00/379064899",
@@ -35,6 +38,15 @@ module.exports = [
     "image": "apples_oranges.jpg",
     "type": "chapter"
   },
+  /* Part: implementing kanban */
+  {
+    "author": "Radek Orszewski",
+    "source": "https://www.flickr.com/photos/61745819@N00/10588140955",
+    "license": "CC BY-NC-ND",
+    "image": "kanban_photo.jpg",
+    "type": "part"
+  },
+  /* react and flux */
   {
     "author": "jeremyg3030",
     "source": "https://www.flickr.com/photos/126433814@N04/16298563416",
@@ -43,6 +55,7 @@ module.exports = [
     "demo": "05_react_and_flux",
     "type": "chapter"
   },
+  /* implementing persistency */
   {
     "author": "Unsplash",
     "source": "https://pixabay.com/en/hitchhiker-thumb-hoodie-backpack-691581/",
@@ -50,6 +63,7 @@ module.exports = [
     "image": "hoodie.jpg",
     "type": "chapter"
   },
+  /* from notes to kanban */
   {
     "author": "Radek Orszewski",
     "source": "https://www.flickr.com/photos/61745819@N00/10588140955",
@@ -57,6 +71,7 @@ module.exports = [
     "image": "kanban_photo.jpg",
     "type": "chapter"
   },
+  /* drag and drop */
   {
     "author": "Emma Story",
     "source": "https://www.flickr.com/photos/15146367@N00/4083260446",
@@ -65,6 +80,7 @@ module.exports = [
     "demo": "07_implementing_dnd",
     "type": "chapter"
   },
+  /* Part: advanced techniques */
   {
     "author": "Jairus Khan",
     "source": "https://www.flickr.com/photos/10282509@N00/4674249352",
@@ -72,6 +88,7 @@ module.exports = [
     "image": "mainframe.jpg",
     "type": "part"
   },
+  /* testing react */
   {
     "author": "skeeze",
     "source": "https://pixabay.com/en/eyeglasses-exam-optometry-vision-679696/",
@@ -81,6 +98,7 @@ module.exports = [
     "endSource": "09_testing_react/kanban_app",
     "type": "chapter"
   },
+  /* typing with react */
   {
     "author": "Markus Mayer",
     "source": "https://www.flickr.com/photos/10832246@N06/2925254789/",
@@ -90,6 +108,7 @@ module.exports = [
     "endSource": "10_typing_with_react",
     "type": "chapter"
   },
+  /* styling react */
   {
     "author": "Andrea Williams",
     "source": "https://www.flickr.com/photos/82405581@N00/3173065446",
@@ -97,6 +116,7 @@ module.exports = [
     "image": "taxi.jpg",
     "type": "chapter"
   },
+  /* structuring react projects */
   {
     "author": "epicantus",
     "source": "https://pixabay.com/en/clock-time-hour-minute-watch-272483/",
@@ -104,6 +124,7 @@ module.exports = [
     "image": "structure.jpg",
     "type": "chapter"
   },
+  /* Part: appendices */
   {
     "author": "forwimuwi73",
     "source": "https://pixabay.com/en/food-peanut-nuts-624600/",
@@ -111,6 +132,7 @@ module.exports = [
     "image": "peanuts.jpg",
     "type": "part"
   },
+  /* language features */
   {
     "author": "wilhei",
     "source": "https://pixabay.com/en/font-lead-set-book-printing-705667/",
@@ -118,6 +140,7 @@ module.exports = [
     "image": "language.jpg",
     "type": "chapter"
   },
+  /* understanding decorators */
   {
     "author": "Ana_J",
     "source": "https://pixabay.com/en/toy-car-bus-toy-red-tin-car-431356/",
@@ -125,6 +148,7 @@ module.exports = [
     "image": "decorators.jpg",
     "type": "chapter"
   },
+  /* troubleshooting */
   {
     "author": "Edvard Munch",
     "source": "https://www.artsy.net/artwork/edvard-munch-the-scream",
