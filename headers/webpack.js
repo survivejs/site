cff4f92f7f7276a6b75590e0d2cdbb9835d7f76b
Juho Vepsalainen <bebraw@gmail.com>
2017-01-02 14:01:09 +0200

    Set up webpack book header images

--- headers/webpack.js
+++ headers/webpack.js
@@ -1,4 +1,5 @@
 module.exports = [
+  /* introduction */
   {
     "author": "James Jordan",
     "source": "https://www.flickr.com/photos/69826987@N00/1501451576",
@@ -6,6 +7,7 @@ module.exports = [
     "image": "journey.jpg",
     "type": "intro"
   },
+  /* webpack compared */
   {
     "author": "Unsplash",
     "source": "https://pixabay.com/en/hitchhiker-thumb-hoodie-backpack-691581/",
@@ -13,17 +15,20 @@ module.exports = [
     "image": "hoodie.jpg",
     "type": "intro"
   },
+
   /* Developing with Webpack */
   {
     "license": "Public Domain",
     "image": "shape_sorter.jpg",
     "type": "part"
   },
+  /* Getting Started  */
   {
     "license": "Public Domain",
     "image": "jumper.jpg",
     "type": "chapter"
   },
+  /* Splitting Configuration */
   {
     "author": "stevepb",
     "source": "https://pixabay.com/en/axe-chopper-cut-split-hatchet-674841/",
@@ -31,6 +36,7 @@ module.exports = [
     "image": "axe.jpg",
     "type": "chapter"
   },
+  /* Automatic Browser Refresh */
   {
     "author": "PublicDomainPictures",
     "source": "https://pixabay.com/en/splashing-splash-aqua-water-rain-165192/",
@@ -38,64 +44,35 @@ module.exports = [
     "image": "splash.jpg",
     "type": "chapter"
   },
+  /* Configuring HMR */
+  {
+    "author": "DanielBrachlow",
+    "source": "https://pixabay.com/en/flame-fire-inferno-orange-burn-1761916/",
+    "license": "Public Domain",
+    "image": "inferno.jpg",
+    "type": "chapter"
+  },
+  /* Linting */
+  {
+    "author": "Eva the Weaver",
+    "source": "https://www.flickr.com/photos/16324044@N00/10833734686",
+    "license": "CC BY-NC-SA",
+    "image": "lint.jpg",
+    "type": "chapter"
+  },
+
+  /* Handling Styling */
+  {
+    "author": "Philip Brown",
+    "source": "https://www.flickr.com/photos/67478016@N00/379064899",
+    "license": "CC BY-NC-ND",
+    "image": "apples_oranges.jpg",
+    "type": "part"
+  },
+  /* Loading Styles */
   {
     "license": "Public Domain",
     "image": "refresh.jpg",
-    "type": "chapter"
-  },
-  {
-    "author": "Efraimstochter",
-    "source": "https://pixabay.com/en/wheels-machine-installation-art-784865/",
-    "license": "Public Domain",
-    "image": "wheels.jpg",
-    "type": "chapter"
-  },
-  /* Building with Webpack */
-  {
-    "author": "werner22brigitte",
-    "source": "https://pixabay.com/en/rusty-metal-old-machine-185531/",
-    "license": "Public Domain",
-    "image": "machine.jpg",
-    "type": "part"
-  },
-  /* Minifying Build */
-  {
-    "author": "jarmoluk",
-    "source": "https://pixabay.com/en/electric-electricity-building-1080584/",
-    "license": "Public Domain",
-    "image": "production.jpg",
-    "type": "chapter"
-  },
-  /* Setting Environment Variables */
-  {
-    "author": "cocoparisienne",
-    "source": "https://pixabay.com/en/firewood-combs-thread-cutting-1157304/",
-    "license": "Public Domain",
-    "image": "firewood.jpg",
-    "type": "chapter"
-  },
-  /* Splitting Bundles */
-  {
-    "author": "stevepb",
-    "source": "https://pixabay.com/en/books-student-study-education-1012088/",
-    "license": "Public Domain",
-    "image": "bundle.jpg",
-    "type": "chapter"
-  },
-  /* Adding Hashes to Filenames */
-  {
-    "author": "ctowner",
-    "source": "https://pixabay.com/en/root-potato-hash-healthy-vegetable-1112017/",
-    "license": "Public Domain",
-    "image": "hash.jpg",
-    "type": "chapter"
-  },
-  /* Cleaning Build */
-  {
-    "author": "jarmoluk",
-    "source": "https://pixabay.com/en/cleaning-washing-cleanup-the-ilo-268126/",
-    "license": "Public Domain",
-    "image": "cleaning.jpg",
     "type": "chapter"
   },
   /* Separating CSS */
@@ -114,12 +91,102 @@ module.exports = [
     "image": "unused.jpg",
     "type": "chapter"
   },
-  /* Analyzing Build Statistics */
+  /* Linting */
+  {
+    "author": "Eva the Weaver",
+    "source": "https://www.flickr.com/photos/16324044@N00/10833734686",
+    "license": "CC BY-NC-SA",
+    "image": "lint.jpg",
+    "type": "chapter"
+  },
+
+  /* Understanding Loaders */
+  {
+    "author": "condesign",
+    "source": "https://pixabay.com/en/camera-photos-photograph-514992/",
+    "license": "Public Domain",
+    "image": "photos.jpg",
+    "type": "part"
+  },
+  /* Loader Definitions */
   {
     "author": "jpj2000nl",
-    "source": "https://pixabay.com/en/tools-technique-vernier-caliper-1093119/",
+    "source": "https://pixabay.com/en/tools-technique-open-end-wrench-1093117/",
     "license": "Public Domain",
-    "image": "caliper.jpg",
+    "image": "wrench.jpg",
+    "type": "chapter"
+  },
+  /* Loading Images */
+  {
+    "author": "PublicDomainPictures",
+    "source": "https://pixabay.com/en/photo-camera-photography-old-retro-219958/",
+    "license": "Public Domain",
+    "image": "old_camera.jpg",
+    "type": "chapter"
+  },
+  /* Loading Fonts */
+  {
+    "author": "Markus Mayer",
+    "source": "https://www.flickr.com/photos/10832246@N06/2925254789/",
+    "license": "CC BY-NC-ND",
+    "image": "typing.jpg",
+    "type": "chapter"
+  },
+  /* Writing Loaders */
+  {
+    "author": "jannebjo",
+    "source": "https://pixabay.com/en/volvo-wheel-loaders-machine-906771/",
+    "license": "Public Domain",
+    "image": "loader.jpg",
+    "type": "chapter"
+  },
+
+  /* Building with Webpack */
+  {
+    "author": "werner22brigitte",
+    "source": "https://pixabay.com/en/rusty-metal-old-machine-185531/",
+    "license": "Public Domain",
+    "image": "machine.jpg",
+    "type": "part"
+  },
+  /* Enabling Sourcemaps */
+  {
+    "author": "Efraimstochter",
+    "source": "https://pixabay.com/en/wheels-machine-installation-art-784865/",
+    "license": "Public Domain",
+    "image": "wheels.jpg",
+    "type": "chapter"
+  },
+  /* Splitting Bundles */
+  {
+    "author": "stevepb",
+    "source": "https://pixabay.com/en/books-student-study-education-1012088/",
+    "license": "Public Domain",
+    "image": "bundle.jpg",
+    "type": "chapter"
+  },
+  /* Code Splitting */
+  {
+    "author": "Arcaion",
+    "source": "https://pixabay.com/en/walnut-italian-cane-shell-hard-1776466/",
+    "license": "Public Domain",
+    "image": "nuts.jpg",
+    "type": "chapter"
+  },
+  /* Processing with Babel*/
+  {
+    "author": "Tama66",
+    "source": "https://pixabay.com/en/tower-factory-headframe-threatening-1897536/",
+    "license": "Public Domain",
+    "image": "babel.jpg",
+    "type": "chapter"
+  },
+  /* Cleaning Build */
+  {
+    "author": "jarmoluk",
+    "source": "https://pixabay.com/en/cleaning-washing-cleanup-the-ilo-268126/",
+    "license": "Public Domain",
+    "image": "cleaning.jpg",
     "type": "chapter"
   },
   /* Hosting on GitHub Pages */
@@ -130,28 +197,16 @@ module.exports = [
     "image": "boots.jpg",
     "type": "chapter"
   },
-  /* Loading Assets */
+
+  /* Optimizing Build */
   {
-    "author": "condesign",
-    "source": "https://pixabay.com/en/camera-photos-photograph-514992/",
+    "author": "jarmoluk",
+    "source": "https://pixabay.com/en/electric-electricity-building-1080584/",
     "license": "Public Domain",
-    "image": "photos.jpg",
+    "image": "production.jpg",
     "type": "part"
   },
-  {
-    "author": "422737",
-    "source": "https://pixabay.com/en/cd-dvd-edge-demarcation-447955/",
-    "license": "Public Domain",
-    "image": "dvd.jpg",
-    "type": "chapter"
-  },
-  {
-    "author": "jpj2000nl",
-    "source": "https://pixabay.com/en/tools-technique-open-end-wrench-1093117/",
-    "license": "Public Domain",
-    "image": "wrench.jpg",
-    "type": "chapter"
-  },
+  /* Minifying Build */
   {
     "author": "BreaLeaDesigns",
     "source": "https://pixabay.com/en/austin-tx-sign-texas-downtown-713746/",
@@ -159,20 +214,31 @@ module.exports = [
     "image": "skinny.jpg",
     "type": "chapter"
   },
+  /* Setting Environment Variables */
   {
-    "author": "PublicDomainPictures",
-    "source": "https://pixabay.com/en/photo-camera-photography-old-retro-219958/",
+    "author": "cocoparisienne",
+    "source": "https://pixabay.com/en/firewood-combs-thread-cutting-1157304/",
     "license": "Public Domain",
-    "image": "old_camera.jpg",
+    "image": "firewood.jpg",
     "type": "chapter"
   },
+  /* Adding Hashes to Filenames */
   {
-    "author": "Markus Mayer",
-    "source": "https://www.flickr.com/photos/10832246@N06/2925254789/",
-    "license": "CC BY-NC-ND",
-    "image": "typing.jpg",
+    "author": "ctowner",
+    "source": "https://pixabay.com/en/root-potato-hash-healthy-vegetable-1112017/",
+    "license": "Public Domain",
+    "image": "hash.jpg",
     "type": "chapter"
   },
+  /* Analyzing Build Statistics */
+  {
+    "author": "jpj2000nl",
+    "source": "https://pixabay.com/en/tools-technique-vernier-caliper-1093119/",
+    "license": "Public Domain",
+    "image": "caliper.jpg",
+    "type": "chapter"
+  },
+
   /* Advanced Techniques */
   {
     "author": "NASA",
@@ -181,6 +247,7 @@ module.exports = [
     "image": "skydiving.jpg",
     "type": "part"
   },
+  /* Consuming Packages */
   {
     "author": "caja",
     "source": "https://pixabay.com/en/white-chocolate-chocolate-sweet-380702/",
@@ -188,13 +255,7 @@ module.exports = [
     "image": "chunks.jpg",
     "type": "chapter"
   },
-  {
-    "author": "Eva the Weaver",
-    "source": "https://www.flickr.com/photos/16324044@N00/10833734686",
-    "license": "CC BY-NC-SA",
-    "image": "lint.jpg",
-    "type": "chapter"
-  },
+  /* Authoring Packages */
   {
     "author": "Duke University Archives",
     "source": "https://www.flickr.com/photos/19219926@N04/3707001321",
@@ -202,18 +263,53 @@ module.exports = [
     "image": "library.jpg",
     "type": "chapter"
   },
-  {
-    "author": "jannebjo",
-    "source": "https://pixabay.com/en/volvo-wheel-loaders-machine-906771/",
-    "license": "Public Domain",
-    "image": "loader.jpg",
-    "type": "chapter"
-  },
+  /* Configuring React */
   {
     "author": "Marko Heinrich",
     "source": "https://pixabay.com/en/event-technology-light-panel-644537/",
     "license": "Public Domain",
     "image": "panel.jpg",
+    "type": "chapter"
+  },
+
+  /* Appendices */
+  {
+    "author": "forwimuwi73",
+    "source": "https://pixabay.com/en/food-peanut-nuts-624600/",
+    "license": "Public Domain",
+    "image": "peanuts.jpg",
+    "type": "part"
+  },
+  /* Searching with React */
+  {
+    "author": "Pexels",
+    "source": "https://pixabay.com/en/adventure-compass-hand-macro-1850673/",
+    "license": "Public Domain",
+    "image": "compass.jpg",
+    "type": "chapter"
+  },
+  /* Configuring HMR with React */
+  {
+    "author": "DanielBrachlow",
+    "source": "https://pixabay.com/en/flame-fire-inferno-orange-burn-1761916/",
+    "license": "Public Domain",
+    "image": "inferno.jpg",
+    "type": "chapter"
+  },
+  /* Writing ESLint Plugins */
+  {
+    "author": "Ana_J",
+    "source": "https://pixabay.com/en/toy-car-bus-toy-red-tin-car-431356/",
+    "license": "Public Domain",
+    "image": "decorators.jpg",
+    "type": "chapter"
+  },
+  /* Customizing ESLint */
+  {
+    "author": "wilhei",
+    "source": "https://pixabay.com/en/font-lead-set-book-printing-705667/",
+    "license": "Public Domain",
+    "image": "language.jpg",
     "type": "chapter"
   }
 ];
