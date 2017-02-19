6390b9f4a43cebb0fedf3f6c5cf675c0ab979cb8
Juho Vepsalainen <bebraw@gmail.com>
2017-02-19 12:24:58 +0200

    Add Disqus rewrite helper

---
+++ utils/generate-disqus-rewrites.js
@@ -0,0 +1,12 @@
+// TODO: generalize
+const headers = require('../sections/webpack')().redirects;
+
+const prefix = 'https://survivejs.com/webpack/';
+let result = '';
+Object.keys(headers).forEach((k) => {
+  const v = headers[k];
+
+  result += prefix + k + ', ' + prefix + v + ',\n';
+});
+
+console.log(result);
