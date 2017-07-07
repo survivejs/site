8bb4fb76a65c7dae7058054067f9ef32f4ddd62f
Juho Vepsalainen <bebraw@gmail.com>
2017-07-07 19:11:41 +0200

    chore: Update note

--- components/LatestPost.jsx
+++ components/LatestPost.jsx
@@ -15,17 +15,12 @@ export default React.createClass({
         return (
             <div>
                 <blockquote className='latestpost tip'>
-                    <div>From the blog:</div>
                     <a className='latestpost-link' href={`/${post.url}`}>
                         {post.title}
                     </a>
                     <hr />
-                    <a className='latestpost-link' href='https://ti.to/peerigon/webpack-workshop'>
-                        Register for Augsburg workshop (early July)
-                    </a>
-                    <br/>
                     <a className='latestpost-link' href='/clinic/'>
-                        Register for Vienna clinics (late July)
+                        <b>Register</b> for Vienna clinics (late July)
                     </a>
                 </blockquote>
             </div>
