6eb03b34781fe4911099827b4d396cfdcfd0e3a6
Juho Vepsalainen <bebraw@gmail.com>
2017-09-07 11:44:55 +0300

    fix: Clone master branches of the books

--- scripts/fetch-books.sh
+++ scripts/fetch-books.sh
@@ -7,12 +7,12 @@ mkdir -p books/webpack-book
 
 cd books/react-book
 if [ -d ".git" ]; then git pull;
-else git clone https://github.com/survivejs/react-book.git .;
+else git clone -b master https://github.com/survivejs/react-book.git .;
 fi
 
 cd -
 
 cd books/webpack-book
 if [ -d ".git" ]; then git pull;
-else git clone https://github.com/survivejs/webpack-book.git .;
+else git clone -b master https://github.com/survivejs/webpack-book.git .;
 fi
