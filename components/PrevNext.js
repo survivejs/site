d918e64b750bebe192f6a92bcbf6af4dae1edb92
Artem Sapegin <artem@sapegin.ru>
2017-12-14 22:20:00 +0100

    [WIP] Try Emotion

--- components/PrevNext.js
+++ components/PrevNext.js
@@ -1,5 +1,40 @@
 import React from "react";
-import { Link } from "@survivejs/components";
+import styled from "react-emotion";
+import { Link as LinkBase } from "@survivejs/components";
+import t from "../styles/theme";
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: ${t.space.xl};
+`;
+
+const Panel = styled("div")`
+  flex-basis: 300px;
+  flex-grow: 1;
+  padding-left: ${p => (p.next ? t.space.m : undefined)};
+  padding-right: ${p => (p.prev ? t.space.m : undefined)};
+  text-align: ${p => (p.prev ? "left" : "right")};
+`;
+
+const Title = styled("h4")`
+  margin-bottom: ${t.space.xxs};
+  font-weight: bold;
+`;
+
+const Link = styled(LinkBase)`
+  display: block;
+  position: relative;
+
+  &::before {
+    font-family: ${t.font.heading};
+    position: absolute;
+    margin-top: 0.15em;
+    content: "${p => (p.prev ? "←" : "→")}";
+    left: ${p => (p.prev ? "-1.2em" : undefined)};
+    right: ${p => (p.prev ? undefined : "-1.2em")};
+  }
+`;
 
 const PrevNext = ({
   next,
@@ -13,24 +48,24 @@ const PrevNext = ({
   }
 
   return (
-    <div className="new-prevnext">
-      {previous ? (
-        <div className="new-prevnext__prev">
-          <div className="new-prevnext__title">{previousText}</div>
-          <Link className="new-prevnext__link" to={previous.url}>
+    <Container>
+      {!!previous && (
+        <Panel prev>
+          <Title>{previousText}</Title>
+          <Link prev to={previous.url}>
             {getTitle(previous)}
           </Link>
-        </div>
-      ) : null}
-      {next ? (
-        <div className="new-prevnext__next">
-          <div className="new-prevnext__title">{nextText}</div>
-          <Link className="new-prevnext__link" to={next.url}>
+        </Panel>
+      )}
+      {!!next && (
+        <Panel next>
+          <Title>{nextText}</Title>
+          <Link next to={next.url}>
             {getTitle(next)}
           </Link>
-        </div>
-      ) : null}
-    </div>
+        </Panel>
+      )}
+    </Container>
   );
 };
 
