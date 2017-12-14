2690235395db3b2f0ca9bdc8ab7657312d1f8be5
Artem Sapegin <artem@sapegin.ru>
2017-12-14 19:43:51 +0100

    Tweak prev-next navigation in blog
    #65

--- components/index.js
+++ components/index.js
@@ -1,7 +1,8 @@
 import Buy from "./Buy";
 import Footer from "./Footer";
 import Navigation from "./Navigation";
+import PrevNext from "./PrevNext";
 import Search from "./Search";
 import SocialLinks from "./SocialLinks";
 
-export { Buy, Footer, Navigation, Search, SocialLinks };
+export { Buy, Footer, Navigation, PrevNext, Search, SocialLinks };
