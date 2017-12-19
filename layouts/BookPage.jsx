8d3960a9df3419807ac025b177afc8754b965eee
Artem Sapegin <artem@sapegin.ru>
2017-12-19 19:39:39 +0100

    [WIP] Unify blog / book page layout (#81)
    Also makes footer always below the sidebar (#80).
    
    :skull: Needs more testing. I’ve removed a lot of possibly unnecessary code :skull:

--- layouts/BookPage.jsx
+++ layouts/BookPage.jsx
@@ -10,6 +10,7 @@ import {
   Toc,
 } from "@survivejs/components";
 import { SocialLinks } from "../components";
+import Page from "./Page";
 
 const BookPage = ({
   page: {
@@ -23,26 +24,8 @@ const BookPage = ({
     type,
   },
   section,
-}) => (
-  <div className="chapter__wrapper">
-    {headerImage && (
-      <div
-        className="header-image"
-        style={{
-          backgroundImage: `url(${headerImage})`,
-        }}
-      />
-    )}
-
-    <h1 className="post__heading">{title}</h1>
-
-    {headerExtra && (
-      <div
-        className="header-extra"
-        dangerouslySetInnerHTML={{ __html: headerExtra }}
-      />
-    )}
-
+}) => {
+  const toc = (
     <div className="toc-nav__wrapper">
       <div className="toc-nav__section">
         <input id="search" className="toc-nav__search" placeholder="Search" />
@@ -57,67 +40,66 @@ const BookPage = ({
         <Resources resources={resources} />
       </div>
     </div>
+  );
 
-    <div className="chapter">
-      <div className="post__content">
-        <LatestPost section={section} />
+  const footer = (
+    <React.Fragment>
+      <SocialLinks type={type} />
 
-        <Meta demo={demo} endSource={endSource} />
+      <PrevNext
+        previous={previous}
+        next={next}
+        previousText="Previous chapter"
+        nextText="Next chapter"
+        getTitle={page => page.file.title}
+      />
 
-        <div
-          className="chapter-content"
-          dangerouslySetInnerHTML={{ __html: body }}
-        />
+      <blockquote className="tip">
+        {section.name === "webpack" ? (
+          <p>
+            This book is available through{" "}
+            <a href="https://leanpub.com/survivejs-webpack">
+              Leanpub (digital)
+            </a>,{" "}
+            <a href="https://www.amazon.com/dp/9526868803">
+              Amazon (paperback)
+            </a>, and{" "}
+            <a href="https://www.amazon.com/dp/B06XWZZGBS">Kindle (digital)</a>.
+            By purchasing the book you support the development of further
+            content. A part of profit (~30%) goes to Tobias Koppers, the author
+            of webpack.
+          </p>
+        ) : (
+          <p>
+            This book is{" "}
+            <a href="https://leanpub.com/survivejs-react">
+              available through Leanpub
+            </a>. By purchasing the book you support the development of further
+            content.
+          </p>
+        )}
+      </blockquote>
+    </React.Fragment>
+  );
 
-        <SocialLinks type={type} />
+  return (
+    <Page
+      headerImage={headerImage}
+      headerExtra={headerExtra}
+      title={title}
+      sidebar={toc}
+      footer={footer}
+    >
+      <LatestPost section={section} />
 
-        <PrevNext
-          previous={previous}
-          next={next}
-          previousText="Previous chapter"
-          nextText="Next chapter"
-          getTitle={page => page.file.title}
-        />
+      <Meta demo={demo} endSource={endSource} />
 
-        <blockquote className="tip">
-          {section.name === "webpack" ? (
-            <p>
-              This book is available through{" "}
-              <a href="https://leanpub.com/survivejs-webpack">
-                Leanpub (digital)
-              </a>,{" "}
-              <a href="https://www.amazon.com/dp/9526868803">
-                Amazon (paperback)
-              </a>, and{" "}
-              <a href="https://www.amazon.com/dp/B06XWZZGBS">
-                Kindle (digital)
-              </a>. By purchasing the book you support the development of
-              further content. A part of profit (~30%) goes to Tobias Koppers,
-              the author of webpack.
-            </p>
-          ) : (
-            <p>
-              This book is{" "}
-              <a href="https://leanpub.com/survivejs-react">
-                available through Leanpub
-              </a>. By purchasing the book you support the development of
-              further content.
-            </p>
-          )}
-        </blockquote>
-
-        <div id="disqus_thread" />
-      </div>
-    </div>
-
-    <PrevNextMini
-      previous={previous}
-      next={next}
-      getTitle={page => page.file.title}
-    />
-
-    <Disqus shortname="survivejs" />
-  </div>
-);
+      <div
+        className="chapter-content"
+        dangerouslySetInnerHTML={{ __html: body }}
+      />
+    </Page>
+  );
+};
 
 export default BookPage;
---
+++ layouts/Page.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Disqus, PrevNextMini } from "@survivejs/components";
+
+const Page = ({
+  headerImage,
+  headerExtra,
+  title,
+  sidebar,
+  footer,
+  previous,
+  next,
+  children,
+}) => {
+  return (
+    <div className="page">
+      {headerImage && (
+        <div
+          className="header-image"
+          style={{
+            backgroundImage: `url(${headerImage})`,
+          }}
+        />
+      )}
+
+      <h1 className="page__heading">{title}</h1>
+
+      {headerExtra && (
+        <div
+          className="header-extra"
+          dangerouslySetInnerHTML={{ __html: headerExtra }}
+        />
+      )}
+
+      <div className="page__container">
+        <div className="page__sidebar">{sidebar}</div>
+        <div className="page__main">
+          <div className="page__section">{children}</div>
+          <div className="page__section">
+            {footer}
+
+            <div id="disqus_thread" />
+
+            <Disqus shortname="survivejs" />
+          </div>
+
+          <PrevNextMini
+            previous={previous}
+            next={next}
+            getTitle={page => page.file.title}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
