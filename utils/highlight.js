d8c96ad2542c11cb6f8308357e2498dd75875dfe
Juho Vepsalainen <bebraw@gmail.com>
2017-12-22 19:12:46 +0200

    refactor: Replace prism with illuminate
    Far better this way. No need to rely on a custom fork anymore. Maybe
    those missing language definitions can be added.

--- utils/highlight.js
+++ utils/highlight.js
@@ -1,28 +1,82 @@
 /* eslint-disable no-console */
-// Ported from webpack.js.org
-const Prism = require("prismjs");
-const languages = require("prism-languages");
+const { addLanguage, addPlugin, highlight } = require("illuminate-js");
+const { bash } = require("illuminate-js/lib/languages/bash");
+const { css } = require("illuminate-js/lib/languages/css");
+const { diff } = require("illuminate-js/lib/languages/diff");
+const { ini } = require("illuminate-js/lib/languages/ini");
+const { javascript } = require("illuminate-js/lib/languages/javascript");
+const { json } = require("illuminate-js/lib/languages/json");
+const { jsx } = require("illuminate-js/lib/languages/jsx");
+const { less } = require("illuminate-js/lib/languages/less");
+const { makefile } = require("illuminate-js/lib/languages/makefile");
+const { markup } = require("illuminate-js/lib/languages/markup");
+const { scss } = require("illuminate-js/lib/languages/scss");
+const { typescript } = require("illuminate-js/lib/languages/typescript");
+const { yaml } = require("illuminate-js/lib/languages/yaml");
 
-attachPrismHooks(Prism);
+const languages = {
+  bash,
+  css,
+  diff,
+  ini,
+  javascript,
+  js: javascript,
+  json,
+  json5: json,
+  jsx,
+  less,
+  makefile,
+  haskell: markup, // XXX
+  markdown: markup, // XXX
+  html: markup,
+  xml: markup,
+  sass: scss, // XXX
+  scss,
+  typescript,
+  ts: typescript,
+  yaml,
+};
 
-const highlight = Prism.highlight;
+Object.keys(languages).forEach(language => {
+  addLanguage(language, languages[language]);
+});
 
-if (typeof document !== "undefined") {
-  // disable automatic highlight on content loaded
-  const script =
-    document.currentScript ||
-    [].slice.call(document.getElementsByTagName("script")).pop();
-  script.setAttribute("data-manual", "");
-}
+addPlugin(addHook => {
+  addHook("before-highlight", env => {
+    env.code = env.code.replace(/leanpub-start-insert/gi, function(match) {
+      return "_LEANPUB_START_INSERT";
+    });
 
-module.exports = function highlightPrism(code, language = "bash") {
+    env.code = env.code.replace(/leanpub-end-insert/gi, function(match) {
+      return "_LEANPUB_END";
+    });
+
+    env.code = env.code.replace(/leanpub-start-delete/gi, function(match) {
+      return "_LEANPUB_START_DELETE";
+    });
+
+    env.code = env.code.replace(/leanpub-end-delete/gi, function(match) {
+      return "_LEANPUB_END";
+    });
+  });
+
+  addHook("after-highlight", env => {
+    env.highlightedCode = env.highlightedCode
+      .replace(/_LEANPUB_START_INSERT/g, '<div class="leanpub-insert">')
+      .replace(/_LEANPUB_START_DELETE/g, '<div class="leanpub-delete">')
+      .replace(/_LEANPUB_END/g, "</div>");
+  });
+});
+
+// XXX: Note naming to avoid recursion
+module.exports = function highlightIlluminate(code, language = "bash") {
   try {
-    return leanpubify(highlight(code, languages[language]));
+    return highlight(code, language);
   } catch (error) {
     if (!languages[language]) {
-      console.warn("Prism does not support this language: ", language);
+      console.warn("Illuminate does not support this language: ", language);
     } else {
-      console.warn("Prism failed to highlight: ", error);
+      console.warn("Failed to highlight: ", error);
     }
   }
 
@@ -49,12 +103,3 @@ function attachPrismHooks(Prism) {
     });
   });
 }
-
-function leanpubify(code) {
-  return code
-    .replace(/_LEANPUB_START_INSERT/g, '<div class="leanpub-insert">')
-    .replace(/_LEANPUB_START_DELETE/g, '<div class="leanpub-delete">')
-    .replace(/_LEANPUB_END/g, "</div>");
-}
-
-module.exports.leanpubify = leanpubify;
