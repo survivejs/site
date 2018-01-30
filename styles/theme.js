0b577a52294a2fdf34374ebd24b197254819496e
Artem Sapegin <artem@sapegin.ru>
2018-01-30 22:44:14 +0100

    New subscribe form
    #34

--- styles/theme.js
+++ styles/theme.js
@@ -1,4 +1,7 @@
 const space = 8;
+const scaleBase = 12;
+const scaleRatio = 1.25;
+
 module.exports = {
   font: {
     heading: '"Avenir Next", "Helvetica Neue", sans-serif',
@@ -6,6 +9,19 @@ module.exports = {
     small: '"Avenir Next", "Helvetica Neue", sans-serif',
     code:
       '"Input Sans Narrow", "Source Code Pro", "Input Sans", "Inconsolata", monospace',
+  },
+  fontWeight: {
+    normal: 400,
+    bold: 600,
+    fat: 700,
+  },
+  fontSize: {
+    alpha: `${scaleBase * Math.pow(scaleRatio, 5)}px`,
+    beta: `${scaleBase * Math.pow(scaleRatio, 4)}px`,
+    gamma: `${scaleBase * Math.pow(scaleRatio, 3)}px`,
+    delta: `${scaleBase * Math.pow(scaleRatio, 2)}px`,
+    epsilon: `${scaleBase * Math.pow(scaleRatio, 1)}px`,
+    zeta: `${scaleBase * Math.pow(scaleRatio, 0)}px`,
   },
   space: {
     xxs: `${space / 4}px`,
@@ -23,5 +39,19 @@ module.exports = {
     background: "#fff",
     backgroundAlt: "#eee",
     backgroundLight: "#fafafa",
+    action: "#6eedf8",
+    actionAlpha: "rgba(110, 237, 248, 0.2)",
+    actionLight: "#0de1f3",
+    actionDark: "#09b5c4",
+  },
+  border: {
+    width: "2px",
+  },
+  form: {
+    size: "52px",
+  },
+  transition: {
+    speed: "0.3s",
+    easing: "cubic-bezier(.23, 1, .32, 1)",
   },
 };
