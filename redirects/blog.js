e56c665655fc4826c152ef23a814842aff64b7b3
Juho Vepsalainen <bebraw@gmail.com>
2018-03-19 21:57:45 +0100

    chore: Fix slug

--- redirects/blog.js
+++ redirects/blog.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "survivejs-webpack23": "webpack-book-updated-to-webpack-4",
   "survivejs-webpack120": "survivejs-webpack-120",
   "mobservable-interview": "mobx-interview",
   "react-router5": "react-router5-interview",
