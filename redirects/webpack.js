cc6948cd4106b73055a8ea38a57bf714badf4790
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-17 16:52:42 +0200

    chore: Update redirects

--- redirects/webpack.js
+++ redirects/webpack.js
@@ -4,99 +4,144 @@ module.exports = {
   "developing-with-webpack": "developing/",
   "developing-with-webpack/setting-up-npm-install-webpack-plugin":
     "introduction/",
-  "developing-with-webpack/optimizing-development": "optimizing-build/",
-  "developing-with-webpack/getting-started": "developing/getting-started/",
+  "developing-with-webpack/optimizing-development":
+    "optimizing-build/",
+  "developing-with-webpack/getting-started":
+    "developing/getting-started/",
   "developing-with-webpack/automatic-browser-refresh":
     "developing/automatic-browser-refresh/",
-  "developing-with-webpack/configuring-hmr": "appendices/hmr/",
-  "developing-with-webpack/linting": "/maintenance/code-quality/linting/",
+  "developing-with-webpack/configuring-hmr":
+    "appendices/hmr/",
+  "developing-with-webpack/linting":
+    "/maintenance/code-quality/linting/",
   "developing-with-webpack/composing-configuration":
     "developing/composing-configuration/",
   "developing-with-webpack/refreshing-css":
     "developing/automatic-browser-refresh/",
-  "developing-with-webpack/enabling-sourcemaps": "building/source-maps/",
+  "developing-with-webpack/enabling-sourcemaps":
+    "building/source-maps/",
   "developing-with-webpack/splitting-configuration":
     "developing/composing-configuration/",
-  "developing/automatic-browser-refresh": "developing/webpack-dev-server/",
-  "developing/linting": "/maintenance/code-quality/linting/",
+  "developing/webpack-dev-server":
+    "developing/development-server/",
+  "developing/automatic-browser-refresh":
+    "developing/development-server/",
+  "developing/linting":
+    "/maintenance/code-quality/linting/",
   "styling/linting": "/maintenance/code-quality/linting/",
-  "output/bundling-libraries": "/maintenance/packaging/standalone-builds/",
+  "output/bundling-libraries":
+    "/maintenance/packaging/standalone-builds/",
   "building-with-webpack": "building/",
-  "output/library-output": "https://webpack.js.org/configuration/output/",
-  "packages/consuming-techniques": "packaging/standalone-builds/",
+  "output/library-output":
+    "https://webpack.js.org/configuration/output/",
+  "packages/consuming-techniques":
+    "packaging/standalone-builds/",
   "packages/consuming": "/maintenance/packaging/",
   "packages/authoring": "/maintenance/packaging/",
-  "packages/authoring-techniques": "/maintenance/packaging/",
-  "appendices/hmr-with-react": "https://github.com/gaearon/react-hot-loader",
+  "packages/authoring-techniques":
+    "/maintenance/packaging/",
+  "appendices/hmr-with-react":
+    "https://github.com/gaearon/react-hot-loader",
   "appendices/customizing-eslint":
     "/maintenance/appendices/customizing-eslint/",
-  "building-with-webpack/minifying-build": "optimizing/minifying/",
-  "building-with-webpack/processing-with-babel": "loading/javascript/",
-  "building-with-webpack/cleaning-build": "building/tidying-up/",
-  "building-with-webpack/attaching-revision": "building/tidying-up/",
-  "building-with-webpack/source-maps": "building/source-maps/",
-  "building-with-webpack/splitting-bundles": "building/splitting-bundles/",
-  "building-with-webpack/code-splitting": "building/code-splitting/",
+  "building-with-webpack/minifying-build":
+    "optimizing/minifying/",
+  "building-with-webpack/processing-with-babel":
+    "loading/javascript/",
+  "building-with-webpack/cleaning-build":
+    "building/tidying-up/",
+  "building-with-webpack/attaching-revision":
+    "building/tidying-up/",
+  "building-with-webpack/source-maps":
+    "building/source-maps/",
+  "building-with-webpack/splitting-bundles":
+    "building/splitting-bundles/",
+  "building-with-webpack/code-splitting":
+    "building/code-splitting/",
   "building-with-webpack/setting-environment-variables":
     "optimizing/environment-variables/",
   "building-with-webpack/adding-hashes-to-filenames":
     "optimizing/adding-hashes-to-filenames/",
-  "building-with-webpack/separating-css": "styling/separating-css/",
+  "building-with-webpack/separating-css":
+    "styling/separating-css/",
   "building-with-webpack/eliminating-unused-css":
     "styling/eliminating-unused-css/",
   "building-with-webpack/analyzing-build-statistics":
     "optimizing/build-analysis/",
-  "building-with-webpack/hosting-on-github-pages": "techniques/deploying/",
-  "building-with-webpack/enabling-sourcemaps": "building/source-maps/",
+  "building-with-webpack/hosting-on-github-pages":
+    "techniques/deploying/",
+  "building-with-webpack/enabling-sourcemaps":
+    "building/source-maps/",
   "optimizing-build": "optimizing/",
   "optimizing-build/minifying": "optimizing/minifying/",
-  "optimizing-build/tree-shaking": "optimizing/tree-shaking/",
+  "optimizing-build/tree-shaking":
+    "optimizing/tree-shaking/",
   "optimizing-build/setting-environment-variables":
     "optimizing/environment-variables/",
   "optimizing-build/adding-hashes-to-filenames":
     "optimizing/adding-hashes-to-filenames/",
-  "optimizing-build/separating-manifest": "optimizing/separating-manifest/",
-  "optimizing-build/analyzing-build-statistics": "optimizing/build-analysis/",
+  "optimizing-build/separating-manifest":
+    "optimizing/separating-runtime/",
+  "optimizing/separating-manifest/":
+    "optimizing/separating-runtime/",
+  "optimizing-build/analyzing-build-statistics":
+    "optimizing/build-analysis/",
   "optimizing/setting-environment-variables":
     "optimizing/environment-variables/",
-  "optimizing/analyzing-build-statistics": "optimizing/build-analysis",
+  "optimizing/analyzing-build-statistics":
+    "optimizing/build-analysis",
   "developing/configuring-hmr": "appendices/hmr/",
   "handling-styles": "styling/",
   "handling-styles/loading": "styling/loading/",
-  "handling-styles/separating-css": "styling/separating-css/",
+  "handling-styles/separating-css":
+    "styling/separating-css/",
   "handling-styles/autoprefixing": "styling/autoprefixing/",
-  "handling-styles/eliminating-unused-css": "styling/eliminating-unused-css/",
-  "handling-styles/linting": "/maintenance/code-quality/linting/",
+  "handling-styles/eliminating-unused-css":
+    "styling/eliminating-unused-css/",
+  "handling-styles/linting":
+    "/maintenance/code-quality/linting/",
   "loading-assets": "loading/",
-  "loading-assets/formats-supported": "loading/loader-definitions/",
-  "loading-assets/loader-definitions": "loading/loader-definitions/",
+  "loading-assets/formats-supported":
+    "loading/loader-definitions/",
+  "loading-assets/loader-definitions":
+    "loading/loader-definitions/",
   "loading-assets/loading-styles": "styling/loading/",
   "loading-assets/loading-images": "loading/images/",
   "loading-assets/loading-fonts": "loading/fonts/",
   "understanding-loaders": "loading/",
-  "understanding-loaders/loader-definitions": "loading/loader-definitions/",
+  "understanding-loaders/loader-definitions":
+    "loading/loader-definitions/",
   "understanding-loaders/images": "loading/images/",
   "understanding-loaders/fonts": "loading/fonts/",
-  "understanding-loaders/writing-loaders": "extending/loaders/",
+  "understanding-loaders/writing-loaders":
+    "extending/loaders/",
   "extending-webpack": "extending/",
   "extending-webpack/loaders": "extending/loaders/",
   "extending-webpack/plugins": "extending/plugins/",
   "advanced-techniques": "packages/",
-  "advanced-techniques/consuming-packages": "packages/consuming/",
-  "advanced-techniques/authoring-packages": "packages/authoring/",
-  "advanced-techniques/understanding-chunks": "building/bundle-splitting/",
+  "advanced-techniques/consuming-packages":
+    "packages/consuming/",
+  "advanced-techniques/authoring-packages":
+    "packages/authoring/",
+  "advanced-techniques/understanding-chunks":
+    "building/bundle-splitting/",
   "advanced-techniques/linting-in-webpack":
     "/maintenance/code-quality/linting/",
-  "advanced-techniques/writing-loaders": "extending/loaders/",
-  "advanced-techniques/configuring-react": "output/server-side-rendering/",
+  "advanced-techniques/writing-loaders":
+    "extending/loaders/",
+  "advanced-techniques/configuring-react":
+    "output/server-side-rendering/",
   "appendices/configuring-hmr-with-react":
     "https://github.com/gaearon/react-hot-loader",
-  "appendices/writing-eslint-plugins": "appendices/customizing-eslint/",
-  "appendices/dynamic-loading": "techniques/dynamic-loading/",
+  "appendices/writing-eslint-plugins":
+    "appendices/customizing-eslint/",
+  "appendices/dynamic-loading":
+    "techniques/dynamic-loading/",
   "appendices/using-web-workers": "techniques/web-workers/",
   "appendices/i18n": "techniques/i18n/",
   "appendices/deploying": "techniques/deploying/",
   "appendices/configuring-hmr": "appendices/hmr/",
   "why-webpack": "what-is-webpack/",
-  "building/splitting-bundles": "building/bundle-splitting/",
+  "building/splitting-bundles":
+    "building/bundle-splitting/",
 };
--- redirects/webpack_react.js
+++ redirects/webpack_react.js
@@ -3,23 +3,32 @@ module.exports = {
   webpack: "/webpack/introduction/",
   webpack_compared: "/webpack/appendices/comparison/",
   developing_with_webpack: "/webpack/developing/",
-  webpack_and_react: "/webpack/output/server-side-rendering/",
+  webpack_and_react:
+    "/webpack/output/server-side-rendering/",
   react: "/react/getting-started/",
-  implementing_notes: "/react/getting-started/implementing-notes/",
-  react_and_flux: "/react/implementing-kanban/react-and-flux/",
-  from_notes_to_kanban: "/react/implementing-kanban/from-notes-to-kanban/",
-  implementing_dnd: "/react/implementing-kanban/drag-and-drop/",
+  implementing_notes:
+    "/react/getting-started/implementing-notes/",
+  react_and_flux:
+    "/react/implementing-kanban/react-and-flux/",
+  from_notes_to_kanban:
+    "/react/implementing-kanban/from-notes-to-kanban/",
+  implementing_dnd:
+    "/react/implementing-kanban/drag-and-drop/",
   building_kanban: "/webpack/building/",
   advanced: "/react/advanced-techniques/",
-  testing_react: "/react/advanced-techniques/testing-react/",
-  typing_with_react: "/react/advanced-techniques/typing-with-react/",
+  testing_react:
+    "/react/advanced-techniques/testing-react/",
+  typing_with_react:
+    "/react/advanced-techniques/typing-with-react/",
   linting_in_webpack: "/maintenance/code-quality/linting/",
   authoring_libraries: "/maintenance/packaging/",
-  styling_react: "/react/advanced-techniques/styling-react/",
+  styling_react:
+    "/react/advanced-techniques/styling-react/",
   appendices: "/react/appendices/",
   structuring_react_projects:
     "/react/advanced-techniques/structuring-react-projects/",
   language_features: "/react/appendices/language-features/",
-  understanding_decorators: "/react/appendices/understanding-decorators/",
+  understanding_decorators:
+    "/react/appendices/understanding-decorators/",
   troubleshooting: "/react/appendices/troubleshooting/",
 };
