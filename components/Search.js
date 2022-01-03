9dd9e922b3b0a323f4c7afa21516fbec71907472
Juho Vepsäläinen <bebraw@gmail.com>
2022-01-03 11:31:29 +0200

    chore: Inject app id

--- components/Search.js
+++ components/Search.js
@@ -1,9 +1,8 @@
 import React from "react";
 
-import { Algolia } from "@survivejs/components";
-
 // TODO: Inject api keys in a smarter way
 const Search = ({ sectionName }) => {
+  let appId = null;
   let apiKey = null;
   let indexName = null;
 
@@ -14,6 +13,7 @@ const Search = ({ sectionName }) => {
     apiKey = "4b3ffdbff33831aa87c5b1f39889f2af";
     indexName = "survivejs_maintenance";
   } else if (sectionName === "webpack") {
+    appId = "BE4LKLLAJZ";
     apiKey = "c233f26a1e0b4b0c54edbb2f06d85864";
     indexName = "survivejs_webpack";
   } else {
@@ -22,11 +22,40 @@ const Search = ({ sectionName }) => {
 
   return (
     <Algolia
+      appId={appId}
       apiKey={apiKey}
       indexName={indexName}
       inputSelector="#search"
     />
   );
 };
+
+const Algolia = ({
+  appId,
+  apiKey,
+  indexName,
+  inputSelector,
+}) => (
+  <div>
+    <script
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/docsearch.js/2/docsearch.min.js"
+    />
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+        docsearch({
+          appId: '${appId}',
+          apiKey: '${apiKey}',
+          indexName: '${indexName}',
+          inputSelector: '${inputSelector}',
+          debug: false
+        });
+      `,
+      }}
+    />
+  </div>
+);
 
 export default Search;
