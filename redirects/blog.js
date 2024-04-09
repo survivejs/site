22314495b25d92e89871ed72ca711c69e75540b8
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-09 15:43:19 +0300

    chore: Drop old redirects

--- redirects/blog.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "survivejs-webpack-23": "webpack-book-updated-to-webpack-4",
-  "survivejs-webpack120": "survivejs-webpack-120",
-  "mobservable-interview": "mobx-interview",
-  "react-router5": "react-router5-interview",
-  "upcoming-events": "summer-2018-events",
-};
--- redirects/maintenance.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  "code-quality/dependencies": "packaging/managing-dependencies/",
-};
