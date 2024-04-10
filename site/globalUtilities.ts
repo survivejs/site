b5af0011cb21d0fc0be7c2bb77990beae7fd831a
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-10 11:33:43 +0300

    feat: Improve outlook of topic indices

--- site/globalUtilities.ts
+++ site/globalUtilities.ts
@@ -2,9 +2,13 @@ import { urlJoin } from "https://bundle.deno.dev/https://deno.land/x/url_join@1.
 import type { Routes } from "https://deno.land/x/gustwind@v0.66.2/types.ts";
 
 function init({ routes }: { routes: Routes }) {
+  function length(arr: unknown[]) {
+    return arr.length;
+  }
+
   // Add your global page utilities here.
   // Alternatively they can be defined per component.
-  return { urlJoin };
+  return { length, urlJoin };
 }
 
 export { init };
